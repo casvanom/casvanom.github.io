f2c13157e111bb44ea540005eaa5cbe19dc74971
Cas Vanommeslaeghe <cas.vanommeslaeghe@gmail.com>
2016-06-21 22:01:38 +0200

    contact me werkt, vertalingen, reformat css

--- js/contact_me.js
+++ js/contact_me.js
@@ -10,7 +10,6 @@ $(function() {
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
-            var phone = $("input#phone").val();
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
@@ -18,7 +17,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "https://formspree.io/cas.vanommeslaeghe@gmail.com",
                 type: "POST",
                 data: {
                     name: name,
